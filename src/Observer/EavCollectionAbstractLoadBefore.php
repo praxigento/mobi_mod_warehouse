2e16efe4956da8e6ccfb37de6b57ef4f98056c65
Alex Gusev <alex@flancer64.com>
2016-03-21 17:49:14 +0200

    MOBI-254 - Warehouse-Lot-Qty related structures refactoring

--- src/Observer/EavCollectionAbstractLoadBefore.php
+++ src/Observer/EavCollectionAbstractLoadBefore.php
@@ -8,8 +8,6 @@ namespace Praxigento\Warehouse\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Praxigento\Warehouse\Config as Cfg;
-use Praxigento\Warehouse\Lib\Entity\Quantity;
 
 class EavCollectionAbstractLoadBefore implements ObserverInterface {
     const AS_TBL = 'pwq';
@@ -19,16 +17,16 @@ class EavCollectionAbstractLoadBefore implements ObserverInterface {
         $obj = $observer->getCollection();
         if($observer->getCollection() instanceof \Magento\Catalog\Model\ResourceModel\Product\Collection) {
             /** @var  $collection \Magento\Catalog\Model\ResourceModel\Product\Collection */
-            $collection = $observer->getCollection();
-            $rsrc = $collection->getResource();
-            $tblQuant = $rsrc->getTable(Quantity::ENTITY_NAME);
-            $as = self::AS_TBL;
-            $tbl = [ $as => $tblQuant ];
-            $eid = Cfg::E_COMMON_A_ENTITY_ID;
-            $on = Quantity::ATTR_PRODUCT_REF . '=' . $eid;
-            $fields = [ self::AS_FLD_QTY => 'SUM(' . $as . '.' . Quantity::ATTR_TOTAL . ')' ];
-            $collection->joinTable($tbl, $on, $fields, null, 'left');
-            $collection->groupByAttribute($eid);
+            //            $collection = $observer->getCollection();
+            //            $rsrc = $collection->getResource();
+            //            $tblQuant = $rsrc->getTable(Quantity::ENTITY_NAME);
+            //            $as = self::AS_TBL;
+            //            $tbl = [ $as => $tblQuant ];
+            //            $eid = Cfg::E_COMMON_A_ENTITY_ID;
+            //            $on = Quantity::ATTR_PRODUCT_REF . '=' . $eid;
+            //            $fields = [ self::AS_FLD_QTY => 'SUM(' . $as . '.' . Quantity::ATTR_TOTAL . ')' ];
+            //            $collection->joinTable($tbl, $on, $fields, null, 'left');
+            //            $collection->groupByAttribute($eid);
             // $sql = $collection->getSelectSql(true);
         }
         return;
