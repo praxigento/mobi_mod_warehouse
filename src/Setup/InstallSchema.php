3c2fcaeca05571d8a194c7c00426965f73bece35
Alex Gusev <alex@flancer64.com>
2016-03-31 14:07:51 +0300

    MOBI-265 - scheme setup migration

--- src/Setup/InstallSchema.php
+++ src/Setup/InstallSchema.php
@@ -7,12 +7,35 @@ namespace Praxigento\Warehouse\Setup;
 
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
+use Praxigento\Warehouse\Lib\Data\Entity\Lot;
+use Praxigento\Warehouse\Lib\Data\Entity\Quantity;
+use Praxigento\Warehouse\Lib\Data\Entity\Warehouse;
 
 class InstallSchema extends \Praxigento\Core\Setup\Schema\Base
 {
 
     protected function _setup(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        // TODO: Implement _setup() method.
+        /**
+         * Read and parse JSON schema.
+         */
+        $pathToFile = __DIR__ . '/../etc/dem.json';
+        $pathToNode = '/dBEAR/package/Praxigento/package/Warehouse';
+        $demPackage = $this->_toolDem->readDemPackage($pathToFile, $pathToNode);
+
+        /* Warehouse */
+        $entityAlias = Warehouse::ENTITY_NAME;
+        $demEntity = $demPackage['entity']['Warehouse'];
+        $this->_toolDem->createEntity($entityAlias, $demEntity);
+
+        /* Lot */
+        $entityAlias = Lot::ENTITY_NAME;
+        $demEntity = $demPackage['entity']['Lot'];
+        $this->_toolDem->createEntity($entityAlias, $demEntity);
+
+        /* Quant */
+        $entityAlias = Quantity::ENTITY_NAME;
+        $demEntity = $demPackage['entity']['Quantity'];
+        $this->_toolDem->createEntity($entityAlias, $demEntity);
     }
 }
