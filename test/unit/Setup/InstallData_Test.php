2269d44fa678c653eb857f32b5d100f7555737c3
Alex Gusev <alex@flancer64.com>
2016-04-06 18:40:18 +0300

    MOBI-265 - 100% coverage in Core.

--- test/unit/Setup/InstallData_Test.php
+++ test/unit/Setup/InstallData_Test.php
@@ -9,7 +9,7 @@ namespace Praxigento\Warehouse\Setup;
 
 include_once(__DIR__ . '/../phpunit_bootstrap.php');
 
-class InstallData_UnitTest extends \Praxigento\Core\Lib\Test\BaseTestCase {
+class InstallData_UnitTest extends \Praxigento\Core\Lib\Test\BaseMockeryCase {
 
     public function test_constructor() {
         $obj = new InstallData();
