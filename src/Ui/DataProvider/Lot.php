765510d9a2be08946312f21e33d24a42d7601a6d
Alex Gusev <alex@flancer64.com>
2016-04-25 19:49:13 +0300

    MOBI-274 - Add warehouse grid to adminhtml

--- src/Ui/DataProvider/Lot.php
+++ src/Ui/DataProvider/Lot.php
@@ -9,30 +9,15 @@ use Magento\Framework\Api\FilterBuilder;
 use Magento\Framework\Api\Search\SearchCriteriaBuilder;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\UrlInterface;
-use Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider;
 use Magento\Framework\View\Element\UiComponent\DataProvider\Reporting;
 use Magento\Store\Model\StoreManagerInterface;
+use Praxigento\Core\Ui\DataProvider\Base as BaseDataProvider;
 use Praxigento\Warehouse\Repo\Entity\ILot as IRepoEntityLot;
 
-class Lot extends DataProvider
+class Lot extends BaseDataProvider
 {
 
-    const JSON_ATTR_ITEMS = 'items';
-    const JSON_ATTR_TOTAL_RECORDS = 'totalRecords';
-
-    /**#@+
-     * UI XML arguments and default values to configure this component.
-     */
-    const UICD_UPDATE_URL = 'mui/index/render';
-    const UIC_CONFIG = 'config';
-    const UIC_UPDATE_URL = 'update_url';
-    /**#@-*/
-
-    /** @var  IRepoEntityLot */
-    protected $_repo;
-
     /**
-     * Warehouse constructor.
      * @param UrlInterface $url
      * @param IRepoEntityLot $repo
      * @param Reporting $reporting
@@ -40,8 +25,6 @@ class Lot extends DataProvider
      * @param RequestInterface $request
      * @param FilterBuilder $filterBuilder
      * @param FilterBuilder $name
-     * @param string $primaryFieldName
-     * @param string $requestFieldName
      * @param array $meta
      * @param array $data
      */
@@ -53,115 +36,22 @@ class Lot extends DataProvider
         RequestInterface $request,
         FilterBuilder $filterBuilder,
         $name,
-        $primaryFieldName = 'entity_id',
-        $requestFieldName = 'id',
         array $meta = [],
         array $data = []
     ) {
-        /* add default Update URL */
-        if (!isset($data[static::UIC_CONFIG][static::UIC_UPDATE_URL])) {
-            $val = $url->getRouteUrl(static::UICD_UPDATE_URL);
-            $data[static::UIC_CONFIG][static::UIC_UPDATE_URL] = $val;
-        }
         parent::__construct(
-            $name,
-            $primaryFieldName,
-            $requestFieldName,
+            $url,
+            $repo,
             $reporting,
             $searchCriteriaBuilder,
             $request,
             $filterBuilder,
+            $name,
+            'entity_id',
+            'id',
             $meta,
             $data
         );
-        /* post construction setup */
-        $this->_repo = $repo;
     }
 
-    public function addField($field, $alias = null)
-    {
-        return parent::addField($field, $alias);
-    }
-
-    public function addFilter(\Magento\Framework\Api\Filter $filter)
-    {
-        return parent::addFilter($filter);
-    }
-
-    public function addOrder($field, $direction)
-    {
-        return parent::addOrder($field, $direction);
-    }
-
-    public function getData()
-    {
-        $criteria = $this->getSearchCriteria();
-        $pageSize = $criteria->getPageSize();
-        $pageIndx = $criteria->getCurrentPage();
-        $where = null;
-        $order = null;
-        /** @var \Magento\Framework\DB\Select $queryTotal */
-        $queryTotal = $this->_repo->getQueryToSelectCount();
-        $total = $queryTotal->getConnection()->fetchOne($queryTotal);
-        /** @var \Magento\Framework\DB\Select $query */
-        $query = $this->_repo->getQueryToSelect();
-        $query->limitPage($pageIndx, $pageSize);
-        $data = $query->getConnection()->fetchAll($query);
-        $result = [
-            static::JSON_ATTR_TOTAL_RECORDS => $total,
-            static::JSON_ATTR_ITEMS => $data
-        ];
-        return $result;
-    }
-
-    public function getFieldMetaInfo($fieldSetName, $fieldName)
-    {
-        return parent::getFieldMetaInfo($fieldSetName, $fieldName);
-    }
-
-    public function getFieldSetMetaInfo($fieldSetName)
-    {
-        return parent::getFieldSetMetaInfo($fieldSetName);
-    }
-
-    public function getFieldsMetaInfo($fieldSetName)
-    {
-        return parent::getFieldsMetaInfo($fieldSetName);
-    }
-
-    public function getMeta()
-    {
-        return parent::getMeta();
-    }
-
-
-    public function getPrimaryFieldName()
-    {
-        return parent::getPrimaryFieldName();
-    }
-
-    public function getRequestFieldName()
-    {
-        return parent::getRequestFieldName();
-    }
-
-    public function getSearchCriteria()
-    {
-        return parent::getSearchCriteria();
-    }
-
-    public function getSearchResult()
-    {
-        return parent::getSearchResult();
-    }
-
-    public function setConfigData($config)
-    {
-        return parent::setConfigData($config);
-    }
-
-    public function setLimit($offset, $size)
-    {
-        return parent::setLimit($offset, $size);
-    }
 }
