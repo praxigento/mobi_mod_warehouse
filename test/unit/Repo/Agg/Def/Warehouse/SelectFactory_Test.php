2df0baca0b2a9d692cd9f6d49a07eb73e15a2bd2
Alex Gusev <alex@flancer64.com>
2016-05-11 17:32:43 +0300

    MOBI-280 - Unit tests and code coverage after grids (Warehouse module)

---
+++ test/unit/Repo/Agg/Def/Warehouse/SelectFactory_Test.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+namespace Praxigento\Warehouse\Repo\Agg\Def\Warehouse;
+
+include_once(__DIR__ . '/../../../../phpunit_bootstrap.php');
+
+class SelectFactory_UnitTest extends \Praxigento\Core\Test\BaseMockeryCase
+{
+    /** @var  \Mockery\MockInterface */
+    private $mConn;
+    /** @var  \Mockery\MockInterface */
+    private $mResource;
+    /** @var SelectFactory */
+    private $obj;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        /** create mocks */
+        $this->mConn = $this->_mockConn();
+        $this->mResource = $this->_mockResourceConnection($this->mConn);
+        /** setup mocks for constructor */
+        /** create object to test */
+        $this->obj = new SelectFactory (
+            $this->mResource
+        );
+    }
+
+    public function test_constructor()
+    {
+        /** === Call and asserts  === */
+        $this->assertInstanceOf(SelectFactory::class, $this->obj);
+    }
+
+    public function test_getSelectCountQuery()
+    {
+        /** === Setup Mocks === */
+        // $result = $this->_conn->select();
+        $mResult = $this->_mockDbSelect();
+        $this->mConn
+            ->shouldReceive('select')->once()
+            ->andReturn($mResult);
+        // $tblStock = [$asStock => $this->_conn->getTableName(Cfg::ENTITY_MAGE_CATALOGINVENTORY_STOCK)];
+        // $tblWrhs = [$asWrhs => $this->_conn->getTableName(EntityWarehouse::ENTITY_NAME)];
+        $this->mConn
+            ->shouldReceive('getTableName')->twice();
+        // $result->from($tblStock, $cols);
+        $mResult->shouldReceive('from', 'joinLeft');
+        /** === Call and asserts  === */
+        $this->obj->getSelectCountQuery();
+    }
+
+    public function test_getSelectQuery()
+    {
+        /** === Setup Mocks === */
+        // $result = $this->_conn->select();
+        $mResult = $this->_mockDbSelect();
+        $this->mConn
+            ->shouldReceive('select')->once()
+            ->andReturn($mResult);
+        // $tblStock = [$asStock => $this->_conn->getTableName(Cfg::ENTITY_MAGE_CATALOGINVENTORY_STOCK)];
+        // $tblWrhs = [$asWrhs => $this->_conn->getTableName(EntityWarehouse::ENTITY_NAME)];
+        $this->mConn
+            ->shouldReceive('getTableName')->twice();
+        // $result->from($tblStock, $cols);
+        $mResult->shouldReceive('from', 'joinLeft');
+        /** === Call and asserts  === */
+        $this->obj->getSelectQuery();
+    }
+
+}
