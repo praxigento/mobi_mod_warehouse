8406c99b9908b38d29bb10475f690bbce0007cc9
Alex Gusev <alex@flancer64.com>
2016-06-02 17:52:47 +0300

    MOBI-299 - Fix \Praxigento\Warehouse\Observer\SaleItemQty

--- src/Observer/SaleItemQty.php
+++ src/Observer/SaleItemQty.php
@@ -27,22 +27,30 @@ class SaleItemQty implements ObserverInterface
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        /** @var \Magento\Sales\Api\Data\OrderItemInterface $item */
+        /** @var \Magento\Sales\Model\Order\Item $item */
         $item = $observer->getData('item');
-        $saleItemId = $item->getItemId();
-        $prodId = $item->getProductId();
-        $qtyOrdered = $item->getQtyOrdered();
-        /* get stock ID for the customer */
-        $reqStock = new \Praxigento\Warehouse\Service\Customer\Request\GetCurrentStock();
-        $respStock = $this->_callCustomer->getCurrentStock($reqStock);
-        $stockId = $respStock->getStockId();
-        /* register sale item (fragment total qty by lots) */
-        $reqSaleItem = new \Praxigento\Warehouse\Service\QtyDistributor\Request\RegisterForSaleItem();
-        $reqSaleItem->setItemId($saleItemId);
-        $reqSaleItem->setProductId($prodId);
-        $reqSaleItem->setQuantity($qtyOrdered);
-        $reqSaleItem->setStockId($stockId);
-        $this->_callQtyDistributor->registerForSaleItem($reqSaleItem);
+        /* MOBI-299: register lots for newly saved order items */
+        $isNew = $item->isObjectNew();
+        if ($isNew) {
+            $saleItemId = $item->getItemId();
+            $prodId = $item->getProductId();
+            $qtyOrdered = $item->getQtyOrdered();
+            /* get stock ID for the customer */
+            /** @var \Magento\Sales\Model\Order $order */
+            $order = $item->getOrder();
+            $custId = $order->getCustomerId();
+            $reqStock = new \Praxigento\Warehouse\Service\Customer\Request\GetCurrentStock();
+            $reqStock->setCustomerId($custId);
+            $respStock = $this->_callCustomer->getCurrentStock($reqStock);
+            $stockId = $respStock->getStockId();
+            /* register sale item (fragment total qty by lots) */
+            $reqSaleItem = new \Praxigento\Warehouse\Service\QtyDistributor\Request\RegisterForSaleItem();
+            $reqSaleItem->setItemId($saleItemId);
+            $reqSaleItem->setProductId($prodId);
+            $reqSaleItem->setQuantity($qtyOrdered);
+            $reqSaleItem->setStockId($stockId);
+            $this->_callQtyDistributor->registerForSaleItem($reqSaleItem);
+        }
         return;
     }
 }
