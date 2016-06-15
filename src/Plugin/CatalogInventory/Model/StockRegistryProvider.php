e8246ff2aa46966b13ca2dd1574050cfdf578e1c
Alex Gusev <alex@flancer64.com>
2016-06-15 19:12:30 +0300

    MOBI-315 - Product #102 (SKU 102) quantity issue

---
+++ src/Plugin/CatalogInventory/Model/StockRegistryProvider.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\Warehouse\Plugin\CatalogInventory\Model;
+
+
+class StockRegistryProvider
+{
+    /** @var \Magento\CatalogInventory\Api\Data\StockItemInterfaceFactory */
+    protected $_factoryStockItem;
+    /** @var \Magento\CatalogInventory\Api\StockItemCriteriaInterfaceFactory */
+    protected $_factoryStockItemCrit;
+    /** @var  \Magento\CatalogInventory\Api\StockItemRepositoryInterface */
+    protected $_repoStockItem;
+    /** @var  \Magento\CatalogInventory\Model\StockRegistryStorage */
+    protected $_storageStockRegistry;
+
+    public function __construct(
+        \Magento\CatalogInventory\Model\StockRegistryStorage $storageStockRegistry,
+        \Magento\CatalogInventory\Api\Data\StockItemInterfaceFactory $factoryStockItem,
+        \Magento\CatalogInventory\Api\StockItemCriteriaInterfaceFactory $factoryStockItemCrit,
+        \Magento\CatalogInventory\Api\StockItemRepositoryInterface $repoStockItem
+    ) {
+        $this->_storageStockRegistry = $storageStockRegistry;
+        $this->_factoryStockItem = $factoryStockItem;
+        $this->_factoryStockItemCrit = $factoryStockItemCrit;
+        $this->_repoStockItem = $repoStockItem;
+    }
+
+    /**
+     * Detect current stock and get appropriate stock item.
+     *
+     * @param \Magento\CatalogInventory\Model\StockRegistryProvider $subject
+     * @param \Closure $proceed
+     * @param int $productId
+     * @param int $scopeId
+     * @return \Magento\CatalogInventory\Api\Data\StockItemInterface
+     */
+    public function aroundGetStockItem(
+        \Magento\CatalogInventory\Model\StockRegistryProvider $subject,
+        \Closure $proceed,
+        $productId,
+        $scopeId
+    ) {
+        $result = $this->_storageStockRegistry->getStockItem($productId, $scopeId);
+        if (null === $result) {
+            $criteria = $this->_factoryStockItemCrit->create();
+            $criteria->setProductsFilter($productId);
+            $criteria->setStockFilter(2);
+            $collection = $this->_repoStockItem->getList($criteria);
+            $result = current($collection->getItems());
+            if ($result && $result->getItemId()) {
+                $this->_storageStockRegistry->setStockItem($productId, $scopeId, $result);
+            } else {
+                $result = $this->_factoryStockItem->create();
+            }
+        }
+        return $result;
+    }
+
+}
