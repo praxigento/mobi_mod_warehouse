60bc00b5d19c8ecfb2c36430f0e77a696ef1fee5
Alex Gusev <alex@flancer64.com>
2016-07-07 10:26:24 +0300

    MOBI-331 - Update Odoo API data objects (according to the discussed on conference)

--- src/Tool/IStockManager.php
+++ src/Tool/IStockManager.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * Resolve current stock ID (MOBI-311).
- *
  * User: Alex Gusev <alex@flancer64.com>
  */
-
 namespace Praxigento\Warehouse\Tool;
 
-
+/**
+ * Resolve current stock ID (MOBI-311).
+ * 
+ * TODO: MOBI APP IMPL (interface should be implemented on app level).
+ */
 interface  IStockManager
 {
     public function getCurrentStockId();
