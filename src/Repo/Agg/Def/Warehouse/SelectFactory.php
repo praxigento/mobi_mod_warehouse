e974d07d943ee124947f8c95671ec0931e9f05d2
Alex Gusev <alex@flancer64.com>
2016-10-03 18:02:46 +0300

    MOBI-460 - Create base class for SQL Select factories

--- src/Repo/Agg/Def/Warehouse/SelectFactory.php
+++ src/Repo/Agg/Def/Warehouse/SelectFactory.php
@@ -4,28 +4,15 @@
  */
 namespace Praxigento\Warehouse\Repo\Agg\Def\Warehouse;
 
-use Magento\Framework\App\ResourceConnection;
 use Praxigento\Warehouse\Config as Cfg;
 use Praxigento\Warehouse\Data\Agg\Warehouse as AggWarehouse;
 use Praxigento\Warehouse\Data\Entity\Warehouse as EntityWarehouse;
 use Praxigento\Warehouse\Repo\Agg\IWarehouse as IRepoWarehouse;
 
 class SelectFactory
-    implements \Praxigento\Core\Repo\Query\IHasSelect
+    extends \Praxigento\Core\Repo\Agg\BaseSelectFactory
 {
-    /** @var  \Magento\Framework\DB\Adapter\AdapterInterface */
-    protected $_conn;
-    /** @var \Magento\Framework\App\ResourceConnection */
-    protected $_resource;
 
-    public function __construct(
-        ResourceConnection $resource
-    ) {
-        $this->_resource = $resource;
-        $this->_conn = $resource->getConnection();
-    }
-
-    /** @inheritdoc */
     public function getQueryToSelectCount()
     {
         $result = $this->_conn->select();
@@ -38,13 +25,12 @@ class SelectFactory
         $cols = "COUNT(" . Cfg::E_CATINV_STOCK_A_STOCK_ID . ")";
         $result->from($tblStock, $cols);
         /* LEFT JOIN prxgt_wrhs_wrhs */
-        $on = $asWrhs . '.' . EntityWarehouse::ATTR_STOCK_REF . '=' . $asStock . '.' . Cfg::E_CATINV_STOCK_A_STOCK_ID;
+        $cond = $asWrhs . '.' . EntityWarehouse::ATTR_STOCK_REF . '=' . $asStock . '.' . Cfg::E_CATINV_STOCK_A_STOCK_ID;
         $cols = [];
-        $result->joinLeft($tblWrhs, $on, $cols);
+        $result->joinLeft($tblWrhs, $cond, $cols);
         return $result;
     }
 
-    /** @inheritdoc */
     public function getQueryToSelect()
     {
         $result = $this->_conn->select();
@@ -61,13 +47,13 @@ class SelectFactory
         ];
         $result->from($tblStock, $cols);
         /* LEFT JOIN prxgt_wrhs_wrhs */
-        $on = $asWrhs . '.' . EntityWarehouse::ATTR_STOCK_REF . '=' . $asStock . '.' . Cfg::E_CATINV_STOCK_A_STOCK_ID;
+        $cond = $asWrhs . '.' . EntityWarehouse::ATTR_STOCK_REF . '=' . $asStock . '.' . Cfg::E_CATINV_STOCK_A_STOCK_ID;
         $cols = [
             AggWarehouse::AS_CURRENCY => EntityWarehouse::ATTR_CURRENCY,
             AggWarehouse::AS_COUNTRY_CODE => EntityWarehouse::ATTR_COUNTRY_CODE,
             AggWarehouse::AS_NOTE => EntityWarehouse::ATTR_NOTE
         ];
-        $result->joinLeft($tblWrhs, $on, $cols);
+        $result->joinLeft($tblWrhs, $cond, $cols);
         return $result;
     }
 }
