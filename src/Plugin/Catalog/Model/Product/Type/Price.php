e18249d03483f991be3b3d8273e2678bf9e26f5b
Alex Gusev <alex@flancer64.com>
2017-01-23 14:48:22 +0200

    MOBI-582 - Group prices for warehouses

---
+++ src/Plugin/Catalog/Model/Product/Type/Price.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\Warehouse\Plugin\Catalog\Model\Product\Type;
+
+
+class Price
+{
+    /**
+     * Replace product regular price by warehouse group price or warehouse price.
+     *
+     * @param \Magento\Catalog\Model\Product\Type\Price $subject
+     * @param \Closure $proceed
+     * @param \Magento\Catalog\Model\Product $product
+     * @return float
+     */
+    public function aroundGetPrice(
+        \Magento\Catalog\Model\Product\Type\Price $subject,
+        \Closure $proceed,
+        \Magento\Catalog\Model\Product $product
+    ) {
+        $result = $proceed($product);
+        $priceWrhs = $product->getData(\Praxigento\Warehouse\Plugin\Catalog\Model\Layer::AS_ATTR_PRICE_WRHS);
+        $priceWrhsGroup = $product->getData(\Praxigento\Warehouse\Plugin\Catalog\Model\Layer::AS_ATTR_PRICE_WRHS);
+        if ($priceWrhsGroup > 0) {
+            $result = $priceWrhsGroup;
+        } elseif ($priceWrhs > 0) {
+            $result = $priceWrhs;
+        }
+        return $result;
+    }
+}
