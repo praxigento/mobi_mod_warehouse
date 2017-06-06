0acc4945013c8262e97f0a7942ea7bb5253d84ea
Alex Gusev <alex@flancer64.com>
2017-06-06 13:17:26 +0300

    MOBI-748 - Use warehouse currency instead of base currency in Magento

---
+++ src/Plugin/Store/Model/Store.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\Warehouse\Plugin\Store\Model;
+
+
+/**
+ * Replace Magento's base currency code by warehouse currency code.
+ */
+class Store
+{
+    /** @var \Praxigento\Warehouse\Tool\IStockManager */
+    protected $hlpStock;
+    /** @var \Praxigento\Warehouse\Repo\Entity\IWarehouse */
+    protected $repoWrhs;
+
+    public function __construct(
+        \Praxigento\Warehouse\Tool\IStockManager $hlpStock,
+        \Praxigento\Warehouse\Repo\Entity\IWarehouse $repoWrhs
+    ) {
+        $this->hlpStock = $hlpStock;
+        $this->repoWrhs = $repoWrhs;
+    }
+
+    /**
+     * Replace Magento's base currency code by warehouse currency code.
+     *
+     * @param \Magento\Store\Model\Store $subject
+     * @param \Closure $proceed
+     * @return string
+     */
+    public function aroundGetBaseCurrencyCode(
+        \Magento\Store\Model\Store $subject,
+        \Closure $proceed
+    ) {
+        /* call parent method to proceed functionality */
+        $proceed();
+        /* then replace store currency code by warehouse currency code */
+        $storeId = $subject->getId();
+        $stockId = $this->hlpStock->getStockIdByStoreId($storeId);
+        $wrhsDo = $this->repoWrhs->getById($stockId);
+        $curCode = $wrhsDo->getCurrency();
+        $result = $curCode;
+        return $result;
+    }
+
+}
