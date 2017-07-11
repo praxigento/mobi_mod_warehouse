dd2479eb89c68663ebb7d6f418b9c0bc9bc990ac
Alex Gusev <alex@flancer64.com>
2017-07-11 13:38:50 +0300

    MOBI-799 - Missed products on front in RC1.2

--- src/Plugin/CatalogInventory/Model/StockRegistryProvider.php
+++ src/Plugin/CatalogInventory/Model/StockRegistryProvider.php
@@ -8,29 +8,71 @@ namespace Praxigento\Warehouse\Plugin\CatalogInventory\Model;
 
 class StockRegistryProvider
 {
-    /** @var \Magento\CatalogInventory\Api\Data\StockItemInterfaceFactory */
-    protected $_factoryStockItem;
-    /** @var \Magento\CatalogInventory\Api\StockItemCriteriaInterfaceFactory */
-    protected $_factoryStockItemCrit;
-    /** @var  \Magento\CatalogInventory\Api\StockItemRepositoryInterface */
-    protected $_repoStockItem;
-    /** @var  \Magento\CatalogInventory\Model\StockRegistryStorage */
-    protected $_storageStockRegistry;
-    /** @var  \Praxigento\Warehouse\Tool\IStockManager */
-    protected $_toolStockManager;
+    /**
+     * @var \Magento\CatalogInventory\Api\Data\StockItemInterfaceFactory
+     */
+    protected $factStockItem;
+    /**
+     * @var \Magento\CatalogInventory\Api\StockItemCriteriaInterfaceFactory
+     */
+    protected $factStockItemCrit;
+    /**
+     * @var \Magento\CatalogInventory\Api\StockRepositoryInterface
+     */
+    protected $repoStock;
+    /**
+     * @var  \Magento\CatalogInventory\Api\StockItemRepositoryInterface
+     */
+    protected $repoStockItem;
+    /**
+     * @var  \Magento\CatalogInventory\Model\StockRegistryStorage
+     */
+    protected $storeStockRegistry;
+    /**
+     * @var  \Praxigento\Warehouse\Tool\IStockManager
+     */
+    protected $toolStockManager;
 
     public function __construct(
-        \Magento\CatalogInventory\Model\StockRegistryStorage $storageStockRegistry,
-        \Magento\CatalogInventory\Api\Data\StockItemInterfaceFactory $factoryStockItem,
-        \Magento\CatalogInventory\Api\StockItemCriteriaInterfaceFactory $factoryStockItemCrit,
+        \Magento\CatalogInventory\Model\StockRegistryStorage $storeStockRegistry,
+        \Magento\CatalogInventory\Api\Data\StockItemInterfaceFactory $factStockItem,
+        \Magento\CatalogInventory\Api\StockItemCriteriaInterfaceFactory $factStockItemCrit,
+        \Magento\CatalogInventory\Api\StockRepositoryInterface $repoStock,
         \Magento\CatalogInventory\Api\StockItemRepositoryInterface $repoStockItem,
         \Praxigento\Warehouse\Tool\IStockManager $toolStockMan
     ) {
-        $this->_storageStockRegistry = $storageStockRegistry;
-        $this->_factoryStockItem = $factoryStockItem;
-        $this->_factoryStockItemCrit = $factoryStockItemCrit;
-        $this->_repoStockItem = $repoStockItem;
-        $this->_toolStockManager = $toolStockMan;
+        $this->storeStockRegistry = $storeStockRegistry;
+        $this->factStockItem = $factStockItem;
+        $this->factStockItemCrit = $factStockItemCrit;
+        $this->repoStock = $repoStock;
+        $this->repoStockItem = $repoStockItem;
+        $this->toolStockManager = $toolStockMan;
+    }
+
+    /**
+     * MOBI-799: load current stock by default.
+     *
+     * @param \Magento\CatalogInventory\Model\StockRegistryProvider $subject
+     * @param \Closure $proceed
+     * @param int $scopeId it's not $websiteId (as Magento thought), it's warehouse ID.
+     * @return \Magento\CatalogInventory\Api\Data\StockInterface
+     */
+    public function aroundGetStock(
+        \Magento\CatalogInventory\Model\StockRegistryProvider $subject,
+        \Closure $proceed,
+        $scopeId
+    )
+    {
+        // $result = $proceed($scopeId); - don't use dumb code.
+        /* get stock by scopeId (websiteId) from app cache if was cached before */
+        $stock = $this->storeStockRegistry->getStock($scopeId);
+        if (null === $stock) {
+            /* ... or load current stock and save to app cache */
+            $stockId = $this->toolStockManager->getCurrentStockId();
+            $stock = $this->repoStock->get($stockId);
+            $this->storeStockRegistry->setStock($scopeId, $stock);
+        }
+        return $stock;
     }
 
     /**
@@ -49,21 +91,20 @@ class StockRegistryProvider
         $scopeId
     ) {
         // $result = $proceed($productId, $scopeId); // original method will create empty item for stock registry
-        $result = $this->_storageStockRegistry->getStockItem($productId, $scopeId);
+        $result = $this->storeStockRegistry->getStockItem($productId, $scopeId);
         if (null === $result) {
-            $criteria = $this->_factoryStockItemCrit->create();
+            $criteria = $this->factStockItemCrit->create();
             $criteria->setProductsFilter($productId);
-            $stockId = $this->_toolStockManager->getCurrentStockId();
+            $stockId = $this->toolStockManager->getCurrentStockId();
             $criteria->setStockFilter($stockId);
-            $collection = $this->_repoStockItem->getList($criteria);
+            $collection = $this->repoStockItem->getList($criteria);
             $result = current($collection->getItems());
             if ($result && $result->getItemId()) {
-                $this->_storageStockRegistry->setStockItem($productId, $scopeId, $result);
+                $this->storeStockRegistry->setStockItem($productId, $scopeId, $result);
             } else {
-                $result = $this->_factoryStockItem->create();
+                $result = $this->factStockItem->create();
             }
         }
         return $result;
     }
-
 }
