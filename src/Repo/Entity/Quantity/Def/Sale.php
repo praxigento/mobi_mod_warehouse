e17e886859324bbda3cbf88253f4f408de7ca45c
Alex Gusev <alex@flancer64.com>
2017-07-13 18:52:13 +0300

    MOBI-806 - Change \Praxigento\Core\Repo\Def\Entity::get to return data objects

--- src/Repo/Entity/Quantity/Def/Sale.php
+++ src/Repo/Entity/Quantity/Def/Sale.php
@@ -10,29 +10,12 @@ use Praxigento\Warehouse\Repo\Entity\Quantity\ISale as IEntityRepo;
 
 class Sale extends BaseEntityRepo implements IEntityRepo
 {
-    /** @var \Magento\Framework\ObjectManagerInterface */
-    protected $_manObj;
 
     public function __construct(
-        \Magento\Framework\ObjectManagerInterface $manObj,
         \Magento\Framework\App\ResourceConnection $resource,
         \Praxigento\Core\Repo\IGeneric $repoGeneric
     ) {
         parent::__construct($resource, $repoGeneric, Entity::class);
-        $this->_manObj = $manObj;
     }
 
-    /**
-     * Method to get Sale Item by Id.
-     *
-     * @param integer $id
-     * @return \Praxigento\Warehouse\Data\Entity\Quantity\Sale[] or empty array if no data found.
-     */
-    public function getBySaleItemId($id)
-    {
-        $where = Entity::ATTR_SALE_ITEM_REF . '=' . (int)$id;
-        /** @var \Praxigento\Warehouse\Data\Entity\Quantity\Sale[] $result */
-        $result = $this->get($where);
-        return $result;
-    }
 }
--- src/Repo/Entity/Quantity/ISale.php
+++ src/Repo/Entity/Quantity/ISale.php
@@ -7,11 +7,4 @@ namespace Praxigento\Warehouse\Repo\Entity\Quantity;
 
 interface ISale extends \Praxigento\Core\Repo\IEntity
 {
-    /**
-     * Get all lot's quantities by sale item id.
-     *
-     * @param int $id
-     * @return \Praxigento\Warehouse\Data\Entity\Quantity\Sale[]
-     */
-    public function getBySaleItemId($id);
 }
