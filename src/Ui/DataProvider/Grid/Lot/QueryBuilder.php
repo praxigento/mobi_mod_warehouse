9ee569f84e66213a3545a7589f532d3be570fb40
dmitriimakhov <dmitriimakhov@gmail.com>
2017-09-21 16:51:29 +0300

    MOBI-900 # Refactor grids in Warehouse & Odoo modules # Query Builders

---
+++ src/Ui/DataProvider/Grid/Lot/QueryBuilder.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Praxigento\Warehouse\Ui\DataProvider\Grid\Lot;
+
+
+use Praxigento\Warehouse\Repo\Entity\Data\Lot as ELot;
+
+class QueryBuilder
+    extends \Praxigento\Core\Ui\DataProvider\Grid\Query\Builder
+{
+    /**#@+ Tables aliases for external usage ('camelCase' naming) */
+    const AS_LOT = 'clt';
+    /**#@- */
+
+    /**#@+
+     * Aliases for data attributes.
+     */
+    const A_CODE = 'code';
+    const A_EXP_DATE = 'expDate';
+    const A_ID = 'id';
+
+    /**#@- */
+
+    protected function getMapper()
+    {
+        if (is_null($this->mapper)) {
+            $map = [
+                self::A_ID => self::AS_LOT . '.' . ELot::ATTR_ID,
+                self::A_EXP_DATE => self::AS_LOT . '.' . ELot::ATTR_EXP_DATE,
+                self::A_CODE => self::AS_LOT . '.' . ELot::ATTR_CODE
+            ];
+            $this->mapper = new \Praxigento\Core\Repo\Query\Criteria\Def\Mapper($map);
+        }
+        $result = $this->mapper;
+        return $result;
+    }
+
+    protected function getQueryItems()
+    {
+        $result = $this->conn->select();
+        /* define tables aliases for internal usage (in this method) */
+        $asLot = self::AS_LOT;
+
+        /* SELECT FROM cataloginventory_stock */
+        $tbl = $this->resource->getTableName(ELot::ENTITY_NAME);
+        $as = $asLot;
+        $cols = [
+            self::A_ID => ELot::ATTR_ID,
+            self::A_EXP_DATE => ELot::ATTR_EXP_DATE,
+            self::A_CODE => ELot::ATTR_CODE
+        ];
+        $result->from([$as => $tbl], $cols);
+
+        return $result;
+    }
+
+    protected function getQueryTotal()
+    {
+        /* get query to select items */
+        /** @var \Magento\Framework\DB\Select $result */
+        $result = $this->getQueryItems();
+        /* ... then replace "columns" part with own expression */
+        $value = 'COUNT(' . self::AS_LOT . '.' . ELot::ATTR_ID . ')';
+
+        /**
+         * See method \Magento\Framework\DB\Select\ColumnsRenderer::render:
+         */
+        /**
+         * if ($column instanceof \Zend_Db_Expr) {...}
+         */
+        $exp = new \Praxigento\Core\Repo\Query\Expression($value);
+        /**
+         *  list($correlationName, $column, $alias) = $columnEntry;
+         */
+        $entry = [null, $exp, null];
+        $cols = [$entry];
+        $result->setPart('columns', $cols);
+
+        return $result;
+    }
+
+}
---
+++ src/Ui/DataProvider/Grid/Warehouse/QueryBuilder.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Praxigento\Warehouse\Ui\DataProvider\Grid\Warehouse;
+
+use Praxigento\Warehouse\Config as Cfg;
+use Praxigento\Warehouse\Repo\Entity\Data\Warehouse as EWarehouse;
+
+class QueryBuilder
+    extends \Praxigento\Core\Ui\DataProvider\Grid\Query\Builder
+{
+    /**#@+ Tables aliases for external usage ('camelCase' naming) */
+    const AS_STOCK = 'cs';
+    const AS_WRHS = 'pww';
+    /**#@- */
+
+    /**#@+
+     * Aliases for data attributes.
+     */
+    const A_CODE = 'Code';
+    const A_COUNTRY_CODE = 'CountryCode';
+    const A_CURRENCY = 'Currency';
+    const A_ID = 'Id';
+    const A_NOTE = 'Note';
+    const A_WEBSITE_ID = 'WebsiteId';
+
+    /**#@- */
+
+
+    protected function getMapper()
+    {
+        if (is_null($this->mapper)) {
+            $map = [
+                self::A_ID => self::AS_STOCK . '.' . Cfg::E_CATINV_STOCK_A_STOCK_ID,
+                self::A_CODE => self::AS_STOCK . '.' . Cfg::E_CATINV_STOCK_A_STOCK_NAME,
+                self::A_WEBSITE_ID => self::AS_STOCK . '.' . Cfg::E_CATINV_STOCK_A_WEBSITE_ID,
+                self::A_CURRENCY => self::AS_WRHS . '.' . EWarehouse::ATTR_CURRENCY,
+                self::A_COUNTRY_CODE => self::AS_WRHS . '.' . EWarehouse::ATTR_COUNTRY_CODE,
+                self::A_NOTE => self::AS_WRHS . '.' . EWarehouse::ATTR_NOTE
+            ];
+            $this->mapper = new \Praxigento\Core\Repo\Query\Criteria\Def\Mapper($map);
+        }
+        $result = $this->mapper;
+        return $result;
+    }
+
+    protected function getQueryItems()
+    {
+        $result = $this->conn->select();
+        /* define tables aliases for internal usage (in this method) */
+        $asStock = self::AS_STOCK;
+        $asWrhs = self::AS_WRHS;
+
+        /* SELECT FROM cataloginventory_stock */
+        $tbl = $this->resource->getTableName(Cfg::ENTITY_MAGE_CATALOGINVENTORY_STOCK);
+        $as = $asStock;
+        $cols = [
+            self::A_ID => Cfg::E_CATINV_STOCK_A_STOCK_ID,
+            self::A_CODE => Cfg::E_CATINV_STOCK_A_STOCK_NAME,
+            self::A_WEBSITE_ID => Cfg::E_CATINV_STOCK_A_WEBSITE_ID
+        ];
+        $result->from([$as => $tbl], $cols);
+
+        /* LEFT JOIN prxgt_wrhs_wrhs */
+        $tbl = $this->resource->getTableName(EWarehouse::ENTITY_NAME);
+        $as = $asWrhs;
+        $cond = $asWrhs . '.' . EWarehouse::ATTR_STOCK_REF . '=' . $asStock . '.' . Cfg::E_CATINV_STOCK_A_STOCK_ID;
+        $cols = [
+            self::A_CURRENCY => EWarehouse::ATTR_CURRENCY,
+            self::A_COUNTRY_CODE => EWarehouse::ATTR_COUNTRY_CODE,
+            self::A_NOTE => EWarehouse::ATTR_NOTE
+        ];
+        $result->joinLeft([$as => $tbl], $cond, $cols);
+        return $result;
+    }
+
+    protected function getQueryTotal()
+    {
+        /* get query to select items */
+        /** @var \Magento\Framework\DB\Select $result */
+        $result = $this->getQueryItems();
+        /* ... then replace "columns" part with own expression */
+        $value = 'COUNT(' . self::AS_STOCK . '.' . Cfg::E_CATINV_STOCK_A_STOCK_ID . ')';
+
+        /**
+         * See method \Magento\Framework\DB\Select\ColumnsRenderer::render:
+         */
+        /**
+         * if ($column instanceof \Zend_Db_Expr) {...}
+         */
+        $exp = new \Praxigento\Core\Repo\Query\Expression($value);
+        /**
+         *  list($correlationName, $column, $alias) = $columnEntry;
+         */
+        $entry = [null, $exp, null];
+        $cols = [$entry];
+        $result->setPart('columns', $cols);
+        return $result;
+    }
+}
