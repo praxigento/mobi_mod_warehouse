ce241db3a5b5227ad2422098d14f2ccba8cb6d3e
Alex Gusev <alex@flancer64.com>
2018-01-14 18:17:00 +0200

    MOBI-1093 Split "\Praxigento\Odoo\Service\IReplicate" service

--- src/Repo/Entity/Quantity.php
+++ src/Repo/Entity/Quantity.php
@@ -5,13 +5,28 @@
 
 namespace Praxigento\Warehouse\Repo\Entity;
 
-class Quantity extends \Praxigento\Core\App\Repo\Def\Entity
+use Praxigento\Warehouse\Repo\Entity\Data\Quantity as Entity;
+
+class Quantity
+    extends \Praxigento\Core\App\Repo\Def\Entity
 {
     public function __construct(
         \Magento\Framework\App\ResourceConnection $resource,
         \Praxigento\Core\App\Repo\IGeneric $repoGeneric
     ) {
-        parent::__construct($resource, $repoGeneric, \Praxigento\Warehouse\Repo\Entity\Data\Quantity::class);
+        parent::__construct($resource, $repoGeneric, Entity::class);
     }
 
+    /**
+     * Get all lots quantities for Magento stock item.
+     *
+     * @param int $stockItemId
+     * @return Entity[]
+     */
+    public function getByStockItemId($stockItemId)
+    {
+        $where = Entity::ATTR_STOCK_ITEM_REF . '=' . (int)$stockItemId;
+        $result = $this->get($where);
+        return $result;
+    }
 }
