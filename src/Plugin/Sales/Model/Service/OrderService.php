f6f3ee8ecb5e1b666209b4a8039ed9f345bd7081
Alex Gusev <alex@flancer64.com>
2018-02-13 19:13:00 +0200

    MOBI-1144 Transaction for sale cancellation

--- src/Plugin/Sales/Model/Service/OrderService.php
+++ src/Plugin/Sales/Model/Service/OrderService.php
@@ -10,12 +10,16 @@ use Praxigento\Warehouse\Service\Sale\Order\Delete\Request as ARequest;
 
 class OrderService
 {
+    /** @var \Praxigento\Core\App\Transaction\Database\IManager */
+    private $manTrans;
     /** @var \Praxigento\Warehouse\Service\Sale\Order\Delete */
     private $servSaleDelete;
 
     public function __construct(
+        \Praxigento\Core\App\Transaction\Database\IManager $manTrans,
         \Praxigento\Warehouse\Service\Sale\Order\Delete $servSaleDelete
     ) {
+        $this->manTrans = $manTrans;
         $this->servSaleDelete = $servSaleDelete;
     }
 
@@ -25,19 +29,26 @@ class OrderService
      * @param \Closure $proceed
      * @param int $id
      * @return bool
-     * @throws \Exception
+     * @throws \Throwable
      */
     public function aroundCancel(
         \Magento\Sales\Model\Service\OrderService $subject,
         \Closure $proceed,
         $id
     ) {
-        $result = $proceed($id);
-        if ($result === true) {
-            $req = new ARequest();
-            $req->setSaleId($id);
-            $req->setCleanDb(true);
-            $resp = $this->servSaleDelete->exec($req);
+        $def = $this->manTrans->begin();
+        try {
+            $result = $proceed($id);
+            if ($result === true) {
+                $req = new ARequest();
+                $req->setSaleId($id);
+                $req->setCleanDb(true);
+                $this->servSaleDelete->exec($req);
+            }
+            $this->manTrans->commit($def);
+        } finally {
+            /* rollback transaction on exception (nothing is done if transaction was committed before) */
+            $this->manTrans->end($def);
         }
         return $result;
     }
