146b3f13920c09a8d3eb087085823d53ce2ad0a9
Alex Gusev <alex@flancer64.com>
2018-02-28 23:08:04 +0200

    MOBI-1171 Error on "Show out of stock products - Yes"

--- src/Plugin/Catalog/Model/Layer.php
+++ src/Plugin/Catalog/Model/Layer.php
@@ -7,17 +7,19 @@ namespace Praxigento\Warehouse\Plugin\Catalog\Model;
 
 use Praxigento\Warehouse\Config as Cfg;
 use Praxigento\Warehouse\Repo\Entity\Data\Group\Price as EGroupPrice;
+use Praxigento\Warehouse\Repo\Query\Catalog\Model\ResourceModel\Product\Collection\Group\Price\Builder as QBGrpPrice;
 
 class Layer
 {
-    const AS_ATTR_PRICE_WRHS_GROUP = Cfg::A_PROD_PRICE_WRHS_GROUP;
-    const AS_TBL_CATALOGINVENTORY_STOCK_ITEM = 'prxgt_csi';
-    /** see \Magento\CatalogInventory\Model\ResourceModel\Stock\Status::addStockDataToCollection */
-    const AS_TBL_STOCK_STATUS_INDEX = 'stock_status_index';
-    const AS_TBL_WRHS_GROUP_PRICE = 'prxgt_wgp';
+    /** Aliases for tables used in query */
+    const AS_CATALOGINVENTORY_STOCK_ITEM = QBGrpPrice::AS_CATALOGINVENTORY_STOCK_ITEM;
+    const AS_WRHS_GROUP_PRICE = 'prxgt_wgp';
+
+    /** Aliases for attributes used in query */
+    const A_PRICE_WRHS_GROUP = Cfg::A_PROD_PRICE_WRHS_GROUP;
 
     /**
-     * Join warehouse PV data to product collection.
+     * Join warehouse group prices to product collection.
      *
      * @param \Magento\Catalog\Model\Layer $subject
      * @param \Closure $proceed
@@ -33,27 +35,17 @@ class Layer
         $query = $collection->getSelect();
         /* aliases and tables */
         $asPriceIndex = \Magento\Catalog\Model\ResourceModel\Product\Collection::INDEX_TABLE_ALIAS;
-        $asStockStatus = self::AS_TBL_STOCK_STATUS_INDEX;
-        $asStockItem = self::AS_TBL_CATALOGINVENTORY_STOCK_ITEM;
-        $asGroupPrice = self::AS_TBL_WRHS_GROUP_PRICE;
-        $tblStockItem = [$asStockItem => $collection->getTable(Cfg::ENTITY_MAGE_CATALOGINVENTORY_STOCK_ITEM)];
-        $tblGroupPrice = [
-            $asGroupPrice => $collection->getTable(EGroupPrice::ENTITY_NAME)
-        ];
-        /* INNER JOIN cataloginventory_stock_item AS prxgt_csi */
-        $on = $asStockItem . '.' . Cfg::E_CATINV_STOCK_ITEM_A_PROD_ID . '='
-            . $asStockStatus . '.' . Cfg::E_CATINV_STOCK_STATUS_A_PROD_ID;
-        $on .= ' AND ' . $asStockItem . '.' . Cfg::E_CATINV_STOCK_ITEM_A_STOCK_ID . '='
-            . $asStockStatus . '.' . Cfg::E_CATINV_STOCK_STATUS_A_STOCK_ID;
-        $cols = [];
-        $query->joinInner($tblStockItem, $on, $cols);
+        $asStockItem = self::AS_CATALOGINVENTORY_STOCK_ITEM;
+        $asGroupPrice = self::AS_WRHS_GROUP_PRICE;
+        $tblGroupPrice = [$asGroupPrice => $collection->getTable(EGroupPrice::ENTITY_NAME)];
+
         // LEFT JOIN prxgt_wrhs_group_price pwgp
         $on = $asGroupPrice . '.' . EGroupPrice::ATTR_STOCK_ITEM_REF . '='
             . $asStockItem . '.' . Cfg::E_CATINV_STOCK_ITEM_A_ITEM_ID;
         $on .= ' AND ' . $asGroupPrice . '.' . EGroupPrice::ATTR_CUST_GROUP_REF . '='
             . $asPriceIndex . '.' . Cfg::E_CAT_PROD_IDX_A_CUST_GROUP_ID;
         $cols = [
-            self::AS_ATTR_PRICE_WRHS_GROUP => EGroupPrice::ATTR_PRICE
+            self::A_PRICE_WRHS_GROUP => EGroupPrice::ATTR_PRICE
         ];
         $query->joinLeft($tblGroupPrice, $on, $cols);
 
