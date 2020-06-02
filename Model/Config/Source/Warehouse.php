012e2ed2e4545d6e2914b8a640bb647a0ce5826e
Alex Gusev <alex@flancer64.com>
2020-06-02 20:50:23 +0300

    SAN-836 Add selector to Catalog Inventory grid

---
+++ Model/Config/Source/Warehouse.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Authors: Alex Gusev <alex@flancer64.com>
+ * Since: 2020
+ */
+
+namespace Praxigento\Warehouse\Model\Config\Source;
+
+class Warehouse
+    implements \Magento\Framework\Data\OptionSourceInterface
+{
+    /** @var array */
+    private $cache;
+    /** @var \Praxigento\Warehouse\Repo\Dao\Warehouse */
+    private $daoWrhs;
+
+    public function __construct(
+        \Praxigento\Warehouse\Repo\Dao\Warehouse $daoWrhs
+    ) {
+        $this->daoWrhs = $daoWrhs;
+    }
+
+    public function toOptionArray()
+    {
+        if (is_null($this->cache)) {
+            $all = $this->daoWrhs->get();
+            $this->cache = [];
+            /** @var \Praxigento\Warehouse\Repo\Data\Warehouse $one */
+            foreach ($all as $one) {
+                $this->cache[] = ['value' => $one->getStockRef(), 'label' => $one->getCode()];
+            }
+            usort($this->cache, function ($a, $b) {
+                return $a['label'] > $b['label'];
+            });
+        }
+        return $this->cache;
+    }
+}
